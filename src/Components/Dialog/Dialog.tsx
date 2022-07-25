a349b8eae2f261ca3f4adbbdcaa654f3f0ea6c03
Ivan Khoda <ivankhoda@gmail.com>
2022-07-25 23:48:29 +0300

    Minor refactories

--- src/Components/Dialog/Dialog.tsx
+++ src/Components/Dialog/Dialog.tsx
@@ -25,43 +25,40 @@ export const Dialog = () => {
         });
       const conversationDataResponse = await getMessagesData(id);
       const messagesData = await conversationDataResponse.json();
-      setMessages(messagesData.reverse());
+      setMessages(messagesData);
     };
-    if (id) {
-      createSubscription(id);
-    }
+
     getData();
   }, []);
-
-  const handleReceivedMessage = (message: any) => {
-    setMessages([...messages, message]);
-  };
   const createSubscription = (conversationId: string | undefined) => {
     cable.subscriptions.create(
       { channel: "ConversationChannel", conversation: conversationId },
       {
         received: (data) => {
-          handleReceivedMessage(data);
+          setMessages([...messages, data]);
         },
       }
     );
   };
-
-  console.log(messages);
+  if (id) {
+    createSubscription(id);
+  }
   return (
     <div className="dialog">
       <div className="dialog__messages">
         {messages.length > 0
-          ? messages.map((message) => (
-              <Message
-                key={message.id}
-                message={message.message}
-                id={message.id}
-                conversation_id={message.conversation_id}
-                user_id={message.user_id}
-                created_at={message.created_at}
-              />
-            ))
+          ? messages
+              .reverse()
+              .map((message) => (
+                <Message
+                  key={message.id}
+                  message={message.message}
+                  id={message.id}
+                  conversation_id={message.conversation_id}
+                  user_id={message.user_id}
+                  created_at={message.created_at}
+                />
+              ))
           : null}
       </div>
       <MessageForm />
